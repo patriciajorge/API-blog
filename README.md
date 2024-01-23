2f26723b47af1c412da3217cc6d998b760ece4c7
patriciajorge <patriciajorge313@gmail.com>
2024-01-23 19:42:08 -0300

    commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # API-blog
- Este projeto é uma API de Blog desenvolvida em Flask, centrada no gerenciamento de autores e postagens.
+
+API de Blog em Flask
+
+Este projeto é uma API de Blog desenvolvida em Flask, oferecendo funcionalidades básicas para o gerenciamento de autores e postagens. A API é construída com o intuito de fornecer uma estrutura sólida que pode ser expandida conforme necessário para atender aos requisitos específicos do seu projeto.
+
+Funcionalidades Principais
+
+Cadastro de Autores:
+
+* Sistema de registro para novos autores no blog.
+* Cada autor é representado por um nome, senha e e-mail único.
+
+Cadastro de Postagens:
+
+* Facilita a criação de novas postagens.
+* Cada postagem possui um título e está associada a um autor existente.
+
+Autenticação por Token:
+
+* Implementa autenticação de usuários através de tokens JWT.
+* Login é necessário para obter um token de acesso, essencial para operações na API.
+
+Leitura de Conteúdo:
+
+* Permite visualizar todas as postagens disponíveis no blog.
+* Oferece detalhes específicos de uma postagem através do seu ID.
+* Facilita a obtenção de uma lista de todos os autores cadastrados no blog.
+
+Operações de Edição e Exclusão:
+
+* Possui funcionalidades para editar e atualizar informações de postagens existentes.
+* Permite a exclusão de postagens indesejadas.
+* Facilita a edição de informações relacionadas a autores, como nome, e-mail e senha.
+* Oferece a capacidade de excluir autores do sistema.
+
+Como Executar
+
+Instalação de Dependências:
+pip install -r requirements.txt
+
+Execução do Servidor:
+python main.py
+
+O servidor Flask será executado localmente na porta 7777 por padrão.
---
+++ estrutura_banco_de_dados.py
@@ -0,0 +1,53 @@
+# SQLAlchemy
+from flask import Flask # Criar um API flask
+from flask_sqlalchemy import SQLAlchemy # Criar banco de dados
+import os
+
+# Caminho absoluto para o diretório do script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Caminho absoluto para o banco de dados no mesmo diretório que o script
+db_path = os.path.join(script_dir, 'blog.db')
+
+# Criar um API flask
+app = Flask(__name__)
+
+# Criar uma instância de SQLAlchemy
+app.config['SECRET_KEY'] = 'PATY@123' # gerar acesso de autenticação
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path # localização do banco de dados
+
+db = SQLAlchemy(app)
+db:SQLAlchemy
+
+# Definir a estrutura da tabela Postagem
+# id_postagem, titulo, autor
+class Postagem(db.Model):
+    __tablename__ = 'postagem'
+    id_postagem = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String)
+    id_autor = db.Column(db.Integer, db.ForeignKey('autor.id_autor')) # nome da tabela
+
+# Definir a estrutura da tabela Autor
+# id_autor, nome, email, senha, admin, postagens
+class Autor(db.Model):
+    __tablename__ = 'autor'
+    id_autor = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String)
+    email = db.Column(db.String)
+    senha = db.Column(db.String)
+    admin = db.Column(db.Boolean)
+    postagens = db.relationship('Postagem') # nome da classe
+
+# Executar o comando para criar o banco de dados
+def inicializar_banco():
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
+        # Criar usuários adminstradores
+        autor = Autor(nome='paty',email='paty@email.com',senha='123456',admin=True)
+        db.session.add(autor) # adicionar o autor ao banco de dados
+        db.session.commit() # salvar
+
+if __name__ == '__main__':
+    inicializar_banco()
